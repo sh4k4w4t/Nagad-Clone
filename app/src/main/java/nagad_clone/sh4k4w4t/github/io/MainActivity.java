f9f10c57f3d758e3308d4d1a9a269504e4466ef8
shakawat <shakawat_hossain@outlook.com>
2022-11-15 23:35:53 +0600

    no info from developer

--- app/src/main/java/nagad_clone/sh4k4w4t/github/io/MainActivity.java
+++ app/src/main/java/nagad_clone/sh4k4w4t/github/io/MainActivity.java
@@ -3,12 +3,16 @@ package nagad_clone.sh4k4w4t.github.io;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
     }
 }
