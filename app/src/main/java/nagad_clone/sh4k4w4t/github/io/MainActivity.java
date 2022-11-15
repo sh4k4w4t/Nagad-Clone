043bfde117c772068a3c24765a3b241a84cda5bd
shakawat <shakawat_hossain@outlook.com>
2022-11-15 21:57:59 +0600

    Initial commit

---
+++ app/src/main/java/nagad_clone/sh4k4w4t/github/io/MainActivity.java
@@ -0,0 +1,14 @@
+package nagad_clone.sh4k4w4t.github.io;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
